9203add9a04af6ac1031f07f217e223e8ed58f6d
olgatenison <88651744+olgatenison@users.noreply.github.com>
2024-07-17 20:48:55 +0300

    first routing

---
+++ src/app/blog/page.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function page() {
+  return (
+    <div>
+      <h3>post 1</h3>
+      <h3>post 2</h3>
+      <h3>post 3</h3>
+      <h3>post 4</h3>
+      <h3>post 5</h3>
+      <h3>post 6</h3>
+      <h3>post 7</h3>
+      <h3>post 8</h3>
+    </div>
+  );
+}
