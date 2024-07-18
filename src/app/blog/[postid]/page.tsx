92bbde565b88aeab963f26fd6b129ac3dfb7cdf2
olgatenison <88651744+olgatenison@users.noreply.github.com>
2024-07-18 21:31:03 +0300

    dinamic rout

--- src/app/blog/[postid]/page.tsx
+++ src/app/blog/[postid]/page.tsx
@@ -1,5 +1,5 @@
 import React from "react";
 
-export default function page() {
-  return <div>details page</div>;
+export default function page({ params }: { params: { postid: string } }) {
+  return <div>details page with the id: {params.postid}</div>;
 }
