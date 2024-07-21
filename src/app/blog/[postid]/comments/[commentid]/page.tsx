6d789e8b2e3dce058601b0335b4aafb039da88f2
olgatenison <88651744+olgatenison@users.noreply.github.com>
2024-07-21 22:24:38 +0300

    comments page

---
+++ src/app/blog/[postid]/comments/[commentid]/page.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function page({
+  params,
+}: {
+  params: {
+    commentid: string;
+    postid: string;
+  };
+}) {
+  return (
+    <div>
+      Comment {params.commentid} for blog post {params.postid}
+    </div>
+  );
+}
