da4e8b634c43a37cc1bec817b2f44e29149a1431
olgatenison <88651744+olgatenison@users.noreply.github.com>
2024-07-27 20:59:11 +0300

    404 page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function notfound() {
+  return (
+    <div>
+      <h1>Page Not Found</h1>
+      <h2>The Requested Resourse Could Not Be Found</h2>
+    </div>
+  );
+}
