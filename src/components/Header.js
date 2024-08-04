99e41f17511d254b823b4ff627fc573e1d66e266
olgatenison <88651744+olgatenison@users.noreply.github.com>
2024-08-04 21:40:58 +0300

    tilewind

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,9 @@
 import React from "react";
 
 export default function Header() {
-  return <div>Header</div>;
+  return (
+    <div>
+      <h1 className="bg-yellow-200 font-medium container mx-auto">Header</h1>
+    </div>
+  );
 }
