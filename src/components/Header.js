97cc1f88b63c07c594a670d5c8f35fff80bcdb1e
olgatenison <88651744+olgatenison@users.noreply.github.com>
2024-08-08 20:24:43 +0300

    changing hero

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,12 @@
+// "use client";
 import React from "react";
 
 export default function Header() {
   return (
     <div>
-      <h1 className="bg-yellow-200 font-medium container mx-auto">Header</h1>
+      <header>
+        <h2>header</h2>
+      </header>
     </div>
   );
 }
