15806ac70093941f9982a40bf0cc3e0007068574
olgatenison <88651744+olgatenison@users.noreply.github.com>
2024-08-11 20:55:06 +0300

    metadata

--- src/app/blog/[postid]/page.tsx
+++ src/app/blog/[postid]/page.tsx
@@ -1,5 +1,19 @@
 import React from "react";
+import { Metadata } from "next";
 
-export default function page({ params }: { params: { postid: string } }) {
+type Props = {
+  params: {
+    postid: string;
+  };
+};
+
+export const generateMetadata = ({ params }: Props): Metadata => {
+  return {
+    title: `BlogPost ${params.postid}`,
+  };
+};
+
+export default function Page({ params }: Props) {
+  // Используйте Props здесь
   return <div>details page with the id: {params.postid}</div>;
 }
---
+++ src/app/users/thirduser/page.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function page() {
+  return <div className="bg-red-200 h-screen">third user page</div>;
+}
