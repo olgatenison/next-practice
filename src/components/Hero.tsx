e598ec342e7214c2326ac0f2ca2bc7ca2d9ceb54
olgatenison <88651744+olgatenison@users.noreply.github.com>
2024-10-31 20:03:37 +0200

    hero section

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -9,8 +9,7 @@ export default function Hero() {
         </h1>
         <p className="mt-6 text-lg leading-8 text-gray-600">
           Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem
-          cupidatat commodo. Elit sunt amet fugiat veniam occaecat fugiat
-          aliqua.блорпав
+          cupidatat commodo.
         </p>
         <div className="mt-10 flex items-center justify-center gap-x-6">
           <a
